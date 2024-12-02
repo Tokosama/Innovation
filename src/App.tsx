c01364ec2fc3bfcb723482fb9e747f01cc1c0261
Tokosama <anouartoko@gmail.com>
2024-12-02 23:50:27 +0100

    Apport des Ajustements pour l'integration du Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import "./App.css";
 import Home from "./pages/home";
-import Header from './components/Header/Header';
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <Header />
-    </div>
+    <>
+    <Home/>
+    </>
   );
 };
 
