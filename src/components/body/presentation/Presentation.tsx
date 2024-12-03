1672041f230d1733ffc33c75d46ed62b347e6eb2
Tokosama <anouartoko@gmail.com>
2024-12-03 02:30:01 +0100

    Modification  des elements de l'onglet de navigation, et peaufinage du rendu

--- src/components/body/presentation/Presentation.tsx
+++ src/components/body/presentation/Presentation.tsx
@@ -64,18 +64,18 @@ export default function Presentation() {
         </div>
         <div className="grid grid-cols-2 py-16">
           <div className="flex justify-end">
-            <div className="border-2 border-[#FF0E47] text-base flex justify-center  bg-[#FF0E47]  items-center w-9 h-9 ml-2  text-white ">
+            <div className="border-2 border-[#FF0E47] text-base flex justify-center  bg-[#FF0E47]  items-center w-9 h-9 ml-2  text-white  cursor-pointer  ">
               1
             </div>
-            <div className="border-2 border-[#FF0E47] text-base flex justify-center   items-center w-9 h-9 ml-2 ">
+            <div className="border-2 border-[#FF0E47] text-base flex justify-center hover:bg-[#FF0E47] hover:text-white   items-center w-9 h-9 ml-2 cursor-pointer transition duration-500 ">
               2
             </div>
-            <div className="border-2 border-[#FF0E47] text-base flex justify-center   items-center w-9 h-9 ml-2 ">
-              2
+            <div className="border-2 border-[#FF0E47] text-base flex justify-center  hover:bg-[#FF0E47] hover:text-white  items-center w-9 h-9 ml-2 cursor-pointer transition duration-500">
+              3
             </div>
           </div>
           <div className="flex justify-end">
-            <div className="border-2 border-[#FF0E47] text-base flex justify-center   items-center px-7 py-2 ml-2 ">
+            <div className="border-2 border-[#FF0E47] text-base flex justify-center hover:bg-[#FF0E47] hover:text-white cursor-pointer transition duration-500  items-center px-7 py-2 ml-2 ">
               Next<span className="ast-right-arrow">→</span>
             </div>
           </div>
