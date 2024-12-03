426bafd39c7424b775509d98e7f533748f013ab4
Tokosama <anouartoko@gmail.com>
2024-12-03 01:45:37 +0100

    Modification du bacground

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,7 +4,7 @@ import Header from "../components/header/Header";
 export default function Home() {
   return (
     <>
-      <div className="font-body">
+      <div className="font-body bg-[#F5F5F5]">
         <Header />
         <Body />
       </div>
